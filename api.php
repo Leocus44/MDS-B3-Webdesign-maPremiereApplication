ed3676dc3b84b1fc9c2a0087a45c700b194cd5d0
Leocus44 <leo.custodio1@orange.fr>
2023-04-07 12:12:43 +0200

    add cat infos

--- api.php
+++ api.php
@@ -10,13 +10,15 @@
 
 <?php
 
+define('CAT_API_KEY', 'live_e2fiBRxtkwYY7M024Gysx6CBOzY9qAIjNCxTX3osiwxz9hTQF67iMv6uLcR9hG3l');
+
 //Préparation de l'appel d'API
 $curl = curl_init();
 
 
 //Paramétrage de l'appel d'API
 curl_setopt_array($curl, array(
-  CURLOPT_URL => 'https://api.thecatapi.com/v1/images/search',
+  CURLOPT_URL => 'https://api.thecatapi.com/v1/images/search?has_breeds=1',
   CURLOPT_RETURNTRANSFER => true,
   CURLOPT_ENCODING => '',
   CURLOPT_MAXREDIRS => 10,
@@ -25,6 +27,14 @@ curl_setopt_array($curl, array(
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => 'GET',
 ));
+
+//On ajoute la cred d'API obtenue sur le site Cat's API
+// Cela permet d'avoir plus d'informationnalités sur cette API
+$headers = [
+  'x-api-key: '. CAT_API_KEY
+];
+// On ajoute l'information dans les Headers de l'appel d'API
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
 
 // On exécute l'appel d'API
 $response = curl_exec($curl);
@@ -40,11 +50,18 @@ $cat = $responseJson[0];
 // On extrait l'URL de l'image du chat
 $imgUrl = $cat->url;
 // DEBUG : Afficher le contenu d'une variable
-  // var_dump($cat->url);
+$breed = $cat->breeds[0];
+// echo '<pre>';  
+// print_r($breed);
+// echo '</pre>';
 
 ?>
 
-<img src="<?php echo $imgUrl ?>">
+<h1><?php echo $breed->name ?></h1>
+<p><i><?php echo $breed->description ?></i></p>
+<p>Origine :<?php echo $breed->origin ?></p>
+
+<img src="<?php echo $imgUrl ?>" width="400">
 
 </body>
 </html>
